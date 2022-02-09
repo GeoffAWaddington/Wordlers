b260d9fb50c87fbc36a635b8c4f4ca6aee32d36d
GeoffAWaddington <waddingtongeoff@gmail.com>
2022-02-08 20:13:09 -0400

    Tweak

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -8,15 +8,15 @@ type Props = {
 
 export const Cell = ({ value, status }: Props) => {
   const classes = classnames(
-    'w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-2xl rounded dark:text-white',
+    'w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-black text-xl rounded dark:text-white',
     {
       'bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600':
         !status,
       'border-black dark:border-slate-100': value && !status,
-      'bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700':
+      'bg-slate-400 dark:bg-slate-700 text-black border-slate-400 dark:border-slate-700':
         status === 'absent',
       'bg-green-500 text-white border-green-500': status === 'correct',
-      'bg-yellow-300 dark:bg-yellow-300 text-white border-yellow-300 dark:border-yellow-300':
+      'bg-yellow-300 dark:bg-yellow-300 text-black border-yellow-300 dark:border-yellow-300':
         status === 'present',
       'cell-animation': !!value,
     }
--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -19,7 +19,7 @@ export const Key = ({
   onClick,
 }: Props) => {
   const classes = classnames(
-    'flex items-center justify-center rounded mx-0.5 text-2xl cursor-pointer select-none dark:text-white',
+    'flex items-center justify-center rounded mx-0.5 text-black text-xl cursor-pointer select-none dark:text-white',
     {
       'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400':
         !status,
